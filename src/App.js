b94515c8c4aa05441f8430ed7d68ab25e0e01c27
Liam R <liamroberts77@googlemail.com>
2022-03-18 17:00:07 +0000

    added grid styling and budgetcard component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 // import './App.css';
-import { Container, Button, Stack } from 'react-bootstrap';
+import { Container, Button, Stack } from "react-bootstrap";
 
 function App() {
   return (
-    <Container>
-     <Stack direction='horizontal' gap={2} className='mb-4'>
-       <h1 className='me-auto'>CashTrack</h1>
-       <Button variant='primary'>Add Budget</Button>
-       <Button variant='outline-primary'>Add Expense</Button>
-     </Stack>
+    <Container className="my-4">
+      <Stack direction="horizontal" gap={2} className="mb-4">
+        <h1 className="me-auto">CashTrack</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+          gap: "1rem",
+          alignItems: "flex-start",
+        }}
+      >
+        <BudgetCard></BudgetCard>
+      </div>
     </Container>
   );
 }
