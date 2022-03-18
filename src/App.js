69b527c41b833026c78224303a75b2b50e6d2d9b
Liam R <liamroberts77@googlemail.com>
2022-03-18 16:39:25 +0000

    added title and buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import { Container, Stack } from 'react-bootstrap';
+// import './App.css';
+import { Container, Button, Stack } from 'react-bootstrap';
 
 function App() {
   return (
     <Container>
      <Stack direction='horizontal' gap={2} className='mb-4'>
-       
+       <h1 className='me-auto'>CashTrack</h1>
+       <Button variant='primary'>Add Budget</Button>
+       <Button variant='outline-primary'>Add Expense</Button>
      </Stack>
     </Container>
   );
