2c6d2028807cedf544d7ac3c7d83acdbf463f67d
Liam R <liamroberts77@googlemail.com>
2022-03-22 22:15:54 +0000

    added styling to div for amount and max

--- src/components/BudgetCard/BudgetCard.js
+++ src/components/BudgetCard/BudgetCard.js
@@ -6,8 +6,10 @@ const BudgetCard = ({ name, amount, max }) => {
     <Card>
         <Card.Body>
             <Card.Title className="d-flex justify-content-between align-itmes-baseline fw-normal mb-3">
-                <div>{name}</div>
-                <div>{currencyFormatter.format(amount)} / {currencyFormatter.format(max)}</div>
+                <div className="me-2">{name}</div>
+                <div className="d-flex align-items-baseline">{currencyFormatter.format(amount)}
+                    <span className="text-muted fs-6 ms-1"> / {currencyFormatter.format(max)}</span>
+                </div>
             </Card.Title>
         </Card.Body>
     </Card>
