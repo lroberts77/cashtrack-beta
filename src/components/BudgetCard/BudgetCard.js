4e781f5c1c8bb8447272e215aca23f9a3c64bc86
Liam R <liamroberts77@googlemail.com>
2022-03-22 22:01:03 +0000

    imported currencyformatter into budgecard to format amounts

--- src/components/BudgetCard/BudgetCard.js
+++ src/components/BudgetCard/BudgetCard.js
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap";
+import { currencyFormatter } from "../../utils";
 
 const BudgetCard = ({ name, amount, max }) => {
   return (
@@ -6,7 +7,7 @@ const BudgetCard = ({ name, amount, max }) => {
         <Card.Body>
             <Card.Title>
                 <div>{name}</div>
-                <div>{amount} / {max}</div>
+                <div>{currencyFormatter.format(amount)} / {currencyFormatter.format(max)}</div>
             </Card.Title>
         </Card.Body>
     </Card>
